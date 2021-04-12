9a9f3fdf9a9edfeceb37a40c9246beea4f7c5c58
hussein6065 <fuseinial@gmail.com>
2021-04-12 21:34:49 +0000

    fixing bug 9.1

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 include_once './Backend/config/Database.php';
 $database = new Database();
 $conn = $database->connect();
-// include_once './Backend/model/admin.php';
+
+
 
 
 
@@ -24,50 +25,58 @@ if(isset($_POST['Login']))
 	$user = $_POST['username'];
 	$pass = $_POST['password'];
 	$faculty = isset($_POST["faculty"]);
-	session_start();
-	$_SESSION['StudentID'] = "60652022";
-	header('location:dashboard.php');
-// 	if($faculty == '1'){
-// 		// echo '<script language="javascript">
-// 		// alert("You have entered '.$faculty. ' I am here")
-// 		// </script>';
-// 		$query = "select * from Faculty where AshesiEmail = :user AND PasswordC = :pass";
-// 		// $result = $conn->query("select * from Faculty where AshesiEmail = '$user' AND PasswordC = '$pass'");
-// 		$stmt = $conn->prepare($query);
-//         $stmt->bindParam(':user', $user);
-// 		$stmt->bindParam(':pass', $pass);
-//         $result = $stmt->execute();
-// 		if($result->rowCount() > 0)
-// 		{
-// 			session_start();
-// 			$data = $result->fetch(PDO::FETCH_ASSOC);
-// 			$_SESSION['FacultyID'] = $data['FacultyID'];
-// 			echo 'Login in '.$data['FacultyID'];
-// 			// header('location:lecturer_dashboard.php');
-// 		}
-// 	}
-// 	elseif($faculty == "0"){
-// 		$query = "select * from Students where AshesiEmail = :user AND PasswordC = :pass";
-// 		$stmt = $conn->prepare($query);
-//         $stmt->bindParam(':user', $user);
-// 		$stmt->bindParam(':pass', $pass);
-// 		$result = $stmt->execute();
-// 		// $result = $conn->query("select * from Students where AshesiEmail = '$user' AND PasswordC = '$pass'");
-// 		if($result->rowCount() > 0)
-// 		{
-// 			session_start();
-// 			$data = $result->fetch(PDO::FETCH_ASSOC);
-// 			$_SESSION['StudentID'] = $data['StudentID'];
-// 			echo 'Login in '.$data['StudentID'];
-// 			// header('location:dashboard.php');
-// 		}
-// 	}
+
+	if($faculty == '1'){
+		session_start();
+		$data = $result->fetch(PDO::FETCH_ASSOC);
+		$_SESSION['FacultyID'] = "ASF95631";
+		// echo 'Login in '.$data['FacultyID'];
+		header('location:lecturer_dashboard.php');
+		// echo '<script language="javascript">
+		// alert("You have entered '.$faculty. ' I am here")
+		// </script>';
+		// $query = "select * from Faculty where AshesiEmail = :user AND PasswordC = :pass";
+		// // $result = $conn->query("select * from Faculty where AshesiEmail = '$user' AND PasswordC = '$pass'");
+		// $stmt = $conn->prepare($query);
+        // $stmt->bindParam(':user', $user);
+		// $stmt->bindParam(':pass', $pass);
+        // $result = $stmt->execute();
+		// if($result->rowCount() > 0)
+		// {
+		// 	session_start();
+		// 	$data = $result->fetch(PDO::FETCH_ASSOC);
+		// 	$_SESSION['FacultyID'] = $data['FacultyID'];
+		// 	echo 'Login in '.$data['FacultyID'];
+		// 	// header('location:lecturer_dashboard.php');
+		// }
+	}
+	elseif($faculty == "0"){
+		$query = "select * from Students where AshesiEmail = :user AND PasswordC = :pass";
+		$stmt = $conn->prepare($query);
+        $stmt->bindParam(':user', $user);
+		$stmt->bindParam(':pass', $pass);
+		$result = $stmt->execute();
+		session_start();
+		// $data = $result->fetch(PDO::FETCH_ASSOC);
+		$_SESSION['StudentID'] = "60652022";
+		// echo 'Login in '.$data['StudentID'];
+		header('location:dashboard.php');
+		// $result = $conn->query("select * from Students where AshesiEmail = '$user' AND PasswordC = '$pass'");
+		// if($result->rowCount() > 0)
+		// {
+		// 	session_start();
+		// 	$data = $result->fetch(PDO::FETCH_ASSOC);
+		// 	$_SESSION['StudentID'] = $data['StudentID'];
+		// 	echo 'Login in '.$data['StudentID'];
+		// 	// header('location:dashboard.php');
+		// }
+	}
 	
-//    else{
-// 		echo '<script language="javascript">
-// 		alert("You have entered the wrong credentials. Please try again")
-// 		</script>';
-//    }
+   else{
+		echo '<script language="javascript">
+		alert("You have entered the wrong credentials. Please try again")
+		</script>';
+   }
 }
 
 
