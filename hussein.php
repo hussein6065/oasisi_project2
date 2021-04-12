6bccbd66f58541ec8da1de06f890d92f05fcfb8a
hussein6065 <fuseinial@gmail.com>
2021-04-12 20:50:11 +0000

    fixing bug 9.1

--- hussein.php
+++ hussein.php
@@ -1,3 +1,15 @@
 <?php
-    echo 'Hussein';
+    include_once './Backend/config/Database.php';
+    include_once './Backend/model/student.php';
+   
+   
+    $database = new Database();
+    $db = $database->connect();
+    $student = new Students($db);
+    $data = '60652022';
+    $result = $student->getPendingBooking();
+    while ( $row = $result->fetch(PDO::FETCH_ASSOC)){
+        $row['Booked_day'];
+    }
+
 ?>
